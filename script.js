48e5d3c0b0de36160d34158a8a1b45d955746840
jaiflores <jaianny.vivian@gmail.com>
2022-06-17 10:25:35 -0300

    Atividade 3

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 //1 - Criar um loop que conte de 1 até 10 usando FOR
 
 
+for(let number = 1; number <= 10; number ++) {
+    console.log("Repetição com for", number);
+}
+
+
+
 //2 - Criar um loop que conte de 10 até 1 usando WHILE
 
+let numero = 10;
+while( numero >= 1) {
+    console.log("Repetição com while", numero --);    
+}
 
-//3 - Criar um loop que conte todos os números ímpares de 1 até 100
 
+
+
+// //3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+for(let num = 1; num <= 100; num =+2) {
+    console.log("Impar até 100", num);
+}
+
+    
 
 //4- Criar um loop que conte todos os números pares de 0 a 100
+
+for(let par = 0; par <= 100; par =+2) {
+    console.log("Par até 100", par);
+}
